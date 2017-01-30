e54cf42d313be4def900dd4a90addc4dae0fed3a
aninternetof <bradyhurlburt@gmail.com>
2017-01-30 08:37:11 -0500

    Makes rover last_checkin field auto_now

--- mission_control/models.py
+++ mission_control/models.py
@@ -4,7 +4,7 @@ class Rover(models.Model):
     name = models.TextField()
     owner = models.TextField()
     local_ip = models.TextField()
-    last_checkin = models.DateTimeField()
+    last_checkin = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- mission_control/serializers.py
+++ mission_control/serializers.py
@@ -1,7 +1,6 @@
 from .models import Rover
 from rest_framework import serializers
 
-
 class RoverSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Rover
