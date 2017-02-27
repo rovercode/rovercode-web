3eeca027d4056fdb781ede758eece1384c1e0df2
aninternetof <bradyhurlburt@gmail.com>
2017-02-27 16:23:50 -0600

    Uses cd instead of pushd

--- deploy-scripts/start_app.sh
+++ deploy-scripts/start_app.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "In the start_app.sh"
-pushd /home/ubuntu/rovercode-web > /dev/null
+cd /home/ubuntu/rovercode-web > /dev/null
 cp env.example .env
 docker-compose build
 docker-compose up
-popd > /dev/null
