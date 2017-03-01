bafd5314712ef7db69c3d619c7cffe61cf67d360
aninternetof <bradyhurlburt@gmail.com>
2017-03-01 15:09:41 -0500

    Only rename app if it is beta

--- deploy-scripts/rename_app.sh
+++ deploy-scripts/rename_app.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-find /home/ubuntu/rovercode-web/ -path /home/ubuntu/rovercode-web/.git -prune -o -path /home/ubuntu/rovercode-web/.env -prune -o -type f -print0 | xargs -0 sed -i 's/rovercode\.com/beta\.rovercode.com/g'
+if [ -z ${ROVERCODE_WEB_BETA+x} ]; then
+  echo "This is not the beta deployment"
+else
+  find /home/ubuntu/rovercode-web/ -path /home/ubuntu/rovercode-web/.git -prune -o -path /home/ubuntu/rovercode-web/.env -prune -o -type f -print0 | xargs -0 sed -i 's/rovercode\.com/beta\.rovercode.com/g'
+fi
