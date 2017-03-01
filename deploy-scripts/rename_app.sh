71d8111306a552bfbbe56cec43fed55561267eb3
aninternetof <bradyhurlburt@gmail.com>
2017-03-01 15:58:33 -0500

    Changes rename to check public ip against beta.rovercode.com

--- deploy-scripts/rename_app.sh
+++ deploy-scripts/rename_app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [ -z ${ROVERCODE_WEB_BETA+x} ]; then
-  echo "This is not the beta deployment"
-else
+
+our_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+beta_ip=$(dig +short beta.rovercode.com)
+
+if [ "$our_ip" == "$beta_ip" ]; then
   find /home/ubuntu/rovercode-web/ -path /home/ubuntu/rovercode-web/.git -prune -o -path /home/ubuntu/rovercode-web/.env -prune -o -type f -print0 | xargs -0 sed -i 's/rovercode\.com/beta\.rovercode.com/g'
+else
+  echo "This is not the beta deployment"
 fi
