eca3e528214c207867f8673c14da58c8b803be1c
aninternetof <bradyhurlburt@gmail.com>
2017-03-02 22:34:02 -0500

    More thoroughly stop and remove previous containers

--- deploy-scripts/remove_prev_install.sh
+++ deploy-scripts/remove_prev_install.sh
@@ -3,4 +3,6 @@ pushd /home/ubuntu/rovercode-web > /dev/null
 docker-compose down
 popd > /dev/null
 rm -rf /home/ubuntu/rovercode-web
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
