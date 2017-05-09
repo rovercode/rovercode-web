62fe53386e0ab7b7d58be6c3b379cbc8b6ede589
aninternetof <bradyhurlburt@gmail.com>
2017-05-09 12:32:54 -0400

    Fixes comments on mission control with load url tests

--- mission_control/tests/test_urls.py
+++ mission_control/tests/test_urls.py
@@ -18,13 +18,13 @@ class TestMissionControlURLs(TestCase):
             'mission-control:home')
 
     def test_home_load_reverse(self):
-        """mission-control:home should reverse to /mission-control/."""
+        """mission-control:home should reverse to /mission-control/load/1."""
         self.assertEqual(
             reverse('mission-control:home_with_load', kwargs={'bd':1}),
             '/mission-control/load/1/')
 
     def test_home_load_resolve(self):
-        """/mission-control/ should resolve to mission-control:home."""
+        """/mission-control/load/1 should resolve to mission-control:home."""
         match = resolve('/mission-control/load/1/')
         self.assertEqual(match.view_name, 'mission-control:home_with_load')
         self.assertEqual(match.kwargs['bd'], '1')
