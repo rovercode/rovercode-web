28c8453e9746a93c6f885183c8010e43e335cd98
aninternetof <bradyhurlburt@gmail.com>
2017-07-19 21:11:13 -0400

    Adds docstrings to new blog files

--- rovercode_web/blog/apps.py
+++ rovercode_web/blog/apps.py
@@ -1,5 +1,7 @@
+"""Blog apps."""
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    """Configuration for the Blog app."""
     name = 'rovercode_web.blog'
--- rovercode_web/blog/urls.py
+++ rovercode_web/blog/urls.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Blog urls."""
 from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url
