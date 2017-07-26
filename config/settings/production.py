036baf742f85f91ce258c95b84d557ed4d138d64
aninternetof <bradyhurlburt@gmail.com>
2017-07-25 20:10:26 -0400

    Fixes accidental change to beta.rovercode.com in production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -65,7 +65,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['beta.rovercode.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['rovercode.com'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ('gunicorn', )
