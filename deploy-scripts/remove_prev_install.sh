31861b4662260e29c982efebd15e7180addc03dc
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2018-02-26 07:30:01 -0500

    Change script order and don't stop on docker rm errors

--- deploy-scripts/remove_prev_install.sh
+++ deploy-scripts/remove_prev_install.sh
@@ -4,7 +4,9 @@ if [ -d /home/ubuntu/rovercode-web ]; then
     docker-compose down
     popd > /dev/null
     rm -rf /home/ubuntu/rovercode-web
+    set +e
     docker stop $(docker ps -a -q)
     docker rm $(docker ps -a -q)
     docker rmi $(docker images -q)
+    set -e
 fi
