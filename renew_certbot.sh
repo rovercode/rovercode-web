ac57bb035a02f03c3399cb7821dc43d0e3e99d73
Clifton Barnes <clifton.a.barnes@gmail.com>
2018-05-12 23:50:05 -0400

    Remove unused files

--- renew_certbot.sh
+++
@@ -1,3 +0,0 @@
-
-se run --rm --name certbot certbot bash -c "sleep 6 && certbot certonly --standalone -d {{ cookiecutter.domain_name }} --text --agree-tos --email {{ cookiecutter.email }} --server https://acme-v01.api.letsencrypt.org/directory --rsa-key-size 4096 --verbose --keep-until-expiring --standalone-supported-challenges http-01"
-docker exec pearl_nginx_1 nginx -s reload
