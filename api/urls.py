dea72b08f048d150ea950581e5809455926c8bb4
Clifton Barnes <clifton.a.barnes@gmail.com>
2018-06-18 21:15:52 -0400

    Change to DRF interactive docs

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import include, url
+from django.views.generic.base import RedirectView
 from rest_framework import routers
-from rest_framework_swagger.views import get_swagger_view
 from rest_framework_jwt.views import verify_jwt_token
 
 from . import views
@@ -15,7 +15,8 @@ router.register(r'block-diagrams', views.BlockDiagramViewSet)
 
 
 urlpatterns = [
-    url(r'^$', get_swagger_view(title='rovercode API')),
     url(r'^api-token-verify/', verify_jwt_token),
     url(r'^v1/', include(router.urls, namespace='v1')),
+    url(r'^$', RedirectView.as_view(
+        pattern_name='api:v1:api-root', permanent=False)),
 ]
