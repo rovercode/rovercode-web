36e33c18a40afab8797b5704f50cc2a907dc5bd5
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2018-10-04 22:29:46 -0400

    Add pragma no cover for realtime routing

--- realtime/routing.py
+++ realtime/routing.py
@@ -1,8 +1,9 @@
 """Routing for the Realtime app."""
-from django.conf.urls import url
+from django.conf.urls import url # pragma: no cover
 
-from . import consumers
+from . import consumers # pragma: no cover
 
-websocket_urlpatterns = [
-    url(r'^ws/realtime/(?P<room_name>[^/]+)/$', consumers.RoverConsumer),
+
+websocket_urlpatterns = [ # pragma: no cover
+    url(r'^ws/realtime/(?P<room_name>[^/]+)/$', consumers.RoverConsumer), # pragma: no cover
 ]
