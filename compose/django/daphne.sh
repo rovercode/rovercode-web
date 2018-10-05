d7895606cebd9aa02498fc7625791eb0f14504e3
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2018-10-05 15:38:47 -0500

    Remove redundant env variable in daphne startup

--- compose/django/daphne.sh
+++ compose/django/daphne.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-export DJANGO_SETTINGS_MODULE=config.settings.production
 python /app/manage.py migrate
 python /app/manage.py collectstatic --noinput
 /usr/local/bin/daphne -b 0.0.0.0 -p 5000 config.asgi:application
