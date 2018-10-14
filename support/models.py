5ea78bf25f0349b35f39afe56939bc6d0f809512
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2018-10-14 16:42:37 -0400

    Change experience level to int

--- support/models.py
+++ support/models.py
@@ -1,5 +1,6 @@
 """Support models."""
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from oauth2_provider.models import Application
 from rovercode_web.users.models import User
 
@@ -8,7 +9,7 @@ class SupportRequest(models.Model):
     """Attributes to describe a single support request."""
     subject = models.CharField(null=False, max_length=25)
     body = models.CharField(null=False, max_length=2000)
-    experience_level = models.CharField(null=False, max_length=25)
+    experience_level = models.IntegerField(null=False, validators=[MinValueValidator(0), MaxValueValidator(3)])
     category = models.CharField(null=False, max_length=25)
     owner = models.ForeignKey(User)
     creation_time = models.DateTimeField(auto_now=True)
