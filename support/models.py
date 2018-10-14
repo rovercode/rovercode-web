d85ce21a98d30930cf0a870f2b67bd3ed5471c48
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2018-10-14 17:18:50 -0400

    Change support category to choices

--- support/models.py
+++ support/models.py
@@ -6,11 +6,17 @@ from rovercode_web.users.models import User
 
 
 class SupportRequest(models.Model):
+    CATEGORIES = (
+        ("DEBUG", "DEBUG"),
+        ("WHAT_NEXT", "WHAT_NEXT"),
+        ("FUN_IDEAS", "FUN_IDEAS"),
+    )
+
     """Attributes to describe a single support request."""
     subject = models.CharField(null=False, max_length=25)
     body = models.CharField(null=False, max_length=2000)
     experience_level = models.IntegerField(null=False, validators=[MinValueValidator(0), MaxValueValidator(3)])
-    category = models.CharField(null=False, max_length=25)
+    category = models.CharField(null=False, max_length=25, choices=CATEGORIES)
     owner = models.ForeignKey(User)
     creation_time = models.DateTimeField(auto_now=True)
     in_progress = models.BooleanField(null=False, default=False)
