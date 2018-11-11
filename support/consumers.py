0d68a7f4a381fdca1a0060633495332f2d07dfc4
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2018-11-11 11:18:53 -0500

    Block blocked users from support

--- support/consumers.py
+++ support/consumers.py
@@ -1,7 +1,12 @@
 """Consumers for Support app."""
 import json
+import logging
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from middleware.tests.mock_auth_middleware \
+    import MockAuthMiddleware, MOCK_USER_ID
+from .models import SupportRequest
+LOGGER = logging.getLogger(__name__)
 
 
 class SupportConsumer(WebsocketConsumer):
@@ -13,7 +18,6 @@ class SupportConsumer(WebsocketConsumer):
     def connect(self):
         """Handle connections."""
         user = self.scope.get('user')
-        print(user)
         if not user or user.is_anonymous:
             self.close(code=401)
             return
@@ -21,11 +25,25 @@ class SupportConsumer(WebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
+        try:
+            supportRequest = SupportRequest.objects.get(id=int(self.room_name))
+        except (SupportRequest.DoesNotExist, ValueError):
+            self.close(code=404)
+            return
+
+        blocked_user_ids = [user.id for user in supportRequest.owner.blocked_users.all()]
+        if user.id in blocked_user_ids:
+            self.close(code=404)
+            return
+
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
+
+        LOGGER.info("Support: user %s connected to rover %s",
+                    user, self.room_name)
 
         self.accept()
 
--- support/tests/test_support_consumer.py
+++ support/tests/test_support_consumer.py
@@ -5,14 +5,37 @@ from django.conf.urls import url
 from channels.testing import WebsocketCommunicator
 from channels.routing import URLRouter
 from support.consumers import SupportConsumer
+from support.models import SupportRequest
+from rovercode_web.users.models import User
+from mission_control.models import BlockDiagram
+from middleware.tests.mock_auth_middleware \
+    import MockAuthMiddleware, MOCK_USER_ID
 
+@pytest.mark.django_db(transaction=True)
 @pytest.mark.asyncio
 async def test_support_consumer():
     """Test sending a message to the room and having it echo it back."""
-    application = URLRouter([
+    user = User.objects.create(
+        id=MOCK_USER_ID,
+        username='me',
+    )
+    program = BlockDiagram.objects.create(
+        user=user,
+        name='test',
+        content='<xml></xml>'
+    )
+    SupportRequest.objects.create(
+        id=42,
+        subject='Test Subject',
+        body='Test body',
+        experience_level=0,
+        owner=user,
+        program=program,
+    )
+    application = MockAuthMiddleware(URLRouter([
         url(r'^ws/support/(?P<room_name>[^/]+)/$', SupportConsumer),
-    ])
-    communicator = WebsocketCommunicator(application, "/ws/support/foobar/")
+    ]))
+    communicator = WebsocketCommunicator(application, "/ws/support/42/")
     connected, _ = await communicator.connect()
     assert connected
     message = json.dumps({"message": "hello"})
@@ -23,3 +46,34 @@ async def test_support_consumer():
     assert response == message
     # Close
     await communicator.disconnect()
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_support_consumer_blocked_user():
+    """Test sending a message to the room and having it echo it back."""
+    blocked_user = User.objects.create(id=MOCK_USER_ID, username='meanie')
+    user = User.objects.create(
+        id=100,
+        username='me',
+    )
+    user.blocked_users.set([blocked_user])
+    program = BlockDiagram.objects.create(
+            user=user,
+            name='test',
+            content='<xml></xml>'
+    )
+    SupportRequest.objects.create(
+        id=42,
+        subject='Test Subject',
+        body='Test body',
+        experience_level=0,
+        owner=user,
+        program=program,
+    )
+    application = MockAuthMiddleware(URLRouter([
+        url(r'^ws/support/(?P<room_name>[^/]+)/$', SupportConsumer),
+    ]))
+    communicator = WebsocketCommunicator(application, "/ws/support/42/")
+    connected, _ = await communicator.connect()
+    assert not connected
