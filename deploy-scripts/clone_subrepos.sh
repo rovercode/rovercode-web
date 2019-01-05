35d49c8865a18a17e08b679d19e2dd0d5aeb22f3
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2019-01-04 22:06:31 -0500

    Use env variables instead of ip addr

--- deploy-scripts/clone_subrepos.sh
+++ deploy-scripts/clone_subrepos.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-our_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-beta_ip=$(dig +short beta.rovercode.com)
-alpha_ip=$(dig +short alpha.rovercode.com)
-
 pushd /home/ubuntu/rovercode-web/mission_control/static > /dev/null
 rm -rf blockly > /dev/null
 git clone https://github.com/aninternetof/blockly.git
 pushd blockly > /dev/null
-if [ "$our_ip" == "$beta_ip" ]; then
+if [ "$env" == "beta" ]; then
   git checkout development
-elif [ "$our_ip" == "$alpha_ip" ]; then
+elif [ "$env" == "alpha" ]; then
   git checkout alpha
 else
   git checkout master
