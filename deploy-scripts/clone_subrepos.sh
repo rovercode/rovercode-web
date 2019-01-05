a9a4b43a67279dc6d605fba90dba200ffacec4a1
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2019-01-04 23:29:17 -0500

    Use deployment group name instead of env variable

--- deploy-scripts/clone_subrepos.sh
+++ deploy-scripts/clone_subrepos.sh
@@ -4,9 +4,9 @@ pushd /home/ubuntu/rovercode-web/mission_control/static > /dev/null
 rm -rf blockly > /dev/null
 git clone https://github.com/aninternetof/blockly.git
 pushd blockly > /dev/null
-if [ "$env" == "beta" ]; then
+if [ "$DEPLOYMENT_GROUP_NAME" == "beta-rovercode-web" ]; then
   git checkout development
-elif [ "$env" == "alpha" ]; then
+elif [ "$DEPLOYMENT_GROUP_NAME" == "alpha-rovercode-web" ]; then
   git checkout alpha
 else
   git checkout master
--- deploy-scripts/rename_app.sh
+++ deploy-scripts/rename_app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$env" == "beta" ]; then
+if [ "$DEPLOYMENT_GROUP_NAME" == "beta-rovercode-web" ]; then
   find /home/ubuntu/rovercode-web/ -path /home/ubuntu/rovercode-web/.git -prune -o -path /home/ubuntu/rovercode-web/.env -prune -o -type f -print0 | xargs -0 sed -i 's/rovercode\.com/beta\.rovercode.com/g'
-elif [ "$env" == "alpha" ]; then
+elif [ "$DEPLOYMENT_GROUP_NAME" == "alpha-rovercode-web" ]; then
   find /home/ubuntu/rovercode-web/ -path /home/ubuntu/rovercode-web/.git -prune -o -path /home/ubuntu/rovercode-web/.env -prune -o -type f -print0 | xargs -0 sed -i 's/rovercode\.com/alpha\.rovercode.com/g'
 else
   echo "This is not the beta or alpha deployment"
