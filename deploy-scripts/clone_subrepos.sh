1cfe8dfb7732012af83b325e8e268fb534a2305d
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2019-01-04 21:49:05 -0500

    Add alpha to deploy scripts

--- deploy-scripts/clone_subrepos.sh
+++ deploy-scripts/clone_subrepos.sh
@@ -2,6 +2,7 @@
 
 our_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 beta_ip=$(dig +short beta.rovercode.com)
+alpha_ip=$(dig +short alpha.rovercode.com)
 
 pushd /home/ubuntu/rovercode-web/mission_control/static > /dev/null
 rm -rf blockly > /dev/null
@@ -9,6 +10,8 @@ git clone https://github.com/aninternetof/blockly.git
 pushd blockly > /dev/null
 if [ "$our_ip" == "$beta_ip" ]; then
   git checkout development
+elif [ "$our_ip" == "$alpha_ip" ]; then
+  git checkout alpha
 else
   git checkout master
 fi
--- deploy-scripts/rename_app.sh
+++ deploy-scripts/rename_app.sh
@@ -2,9 +2,12 @@
 
 our_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 beta_ip=$(dig +short beta.rovercode.com)
+alpha_ip=$(dig +short alpha.rovercode.com)
 
 if [ "$our_ip" == "$beta_ip" ]; then
   find /home/ubuntu/rovercode-web/ -path /home/ubuntu/rovercode-web/.git -prune -o -path /home/ubuntu/rovercode-web/.env -prune -o -type f -print0 | xargs -0 sed -i 's/rovercode\.com/beta\.rovercode.com/g'
+elif [ "$our_ip" == "$alpha_ip" ]; then
+  find /home/ubuntu/rovercode-web/ -path /home/ubuntu/rovercode-web/.git -prune -o -path /home/ubuntu/rovercode-web/.env -prune -o -type f -print0 | xargs -0 sed -i 's/rovercode\.com/alpha\.rovercode.com/g'
 else
-  echo "This is not the beta deployment"
+  echo "This is not the beta or alpha deployment"
 fi
