862b52301725331811bfb2af19f992d80fe2b8a9
Clifton Barnes <clifton.barnes@rovercode.com>
2019-02-21 21:48:01 -0500

    Restrict rest framework to only token authentication

--- config/settings/common.py
+++ config/settings/common.py
@@ -300,8 +300,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
