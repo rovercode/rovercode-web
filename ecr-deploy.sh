fea4696aae39b274294a37bf2a0699c8d839f20e
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2019-07-06 10:44:51 -0400

    Fix bugs in ecr deploy script

--- ecr-deploy.sh
+++ ecr-deploy.sh
@@ -6,7 +6,10 @@ fi
 
 echo "Deploying $1 image to ECR"
 
-cp /compose/django/Dockerfile .
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+cp ./compose/django/Dockerfile .
 docker build -t 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode:$1 .
 eval $(aws ecr get-login --region us-east-2 --no-include-email) # requires AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY secret environment variable set in Travis
 docker push 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode:$1
