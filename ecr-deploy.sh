5bce16998eaaafc4029aa36b0af6f8e74f893dcd
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2019-07-07 19:14:37 -0400

    Avoid copying Dockerfile in ECR deploy

--- ecr-deploy.sh
+++ ecr-deploy.sh
@@ -8,7 +8,6 @@ echo "Deploying $1 image to ECR"
 
 pip install awscli
 
-cp ./compose/django/Dockerfile .
-docker build -t 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode-web-service:$1 .
+docker build -t 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode-web-service:$1 -f ./compose/django/Dockerfile .
 eval $(aws ecr get-login --region us-east-2 --no-include-email) # requires AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY secret environment variable set in Travis
 docker push 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode-web-service:$1
