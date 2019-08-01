5ea231d1fe57959e697dde599e2c850dd098581e
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2019-07-31 23:36:13 -0400

    Delete redis URL env variable from entrypoint

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -6,7 +6,6 @@ cmd="$@"
 # Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
 # environment variables just to support cookiecutter out of the box. That makes no sense, so this little entrypoint
 # does all this for us.
-export REDIS_URL=redis://redis:6379
 
 # the official postgres image uses 'postgres' as default user if not set explictly.
 if [ -z "$POSTGRES_USER" ]; then
