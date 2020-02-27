eb2299d5eb55cd4a35f2ec750eb7c9fd6b9dc50d
Clifton Barnes <clifton.barnes@rovercode.com>
2020-02-26 22:17:43 -0500

    Enable ECR deploy

--- ecr-deploy.sh
+++ ecr-deploy.sh
@@ -9,5 +9,7 @@ echo "Deploying $1 image to ECR"
 pip install awscli
 
 docker build -t 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode-web-service:$1 -f ./compose/django/Dockerfile .
+docker build -t 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode-profanity-check:$1 -f ./compose/profanity/Dockerfile .
 eval $(aws ecr get-login --region us-east-2 --no-include-email) # requires AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY secret environment variable set in Travis
 docker push 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode-web-service:$1
+docker push 795223264977.dkr.ecr.us-east-2.amazonaws.com/rovercode-profanity-check:$1
