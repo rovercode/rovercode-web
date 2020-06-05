c8626aa40bd33afae1e360fbeaf42df604f93b12
Clifton Barnes <clifton.barnes@rovercode.com>
2020-06-04 22:51:28 -0400

    Remove unused error templates

--- config/urls.py
+++ config/urls.py
@@ -28,14 +28,6 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
-        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
-        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
-        url(r'^500/$', default_views.server_error),
-    ]
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
 
